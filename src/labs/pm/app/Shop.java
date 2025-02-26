9cb80710d7e6514785e7da3fa53d45a1e728ef41
kawtar <kawtar.karkour@gmail.com>
2025-02-26 00:00:50 +0000

    add text parser operation

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -2,6 +2,7 @@ package labs.pm.app;
 
 import labs.pm.data.Product;
 import labs.pm.data.ProductManager;
+import labs.pm.data.ProductManagerException;
 import labs.pm.data.Rating;
 
 import java.math.BigDecimal;
@@ -15,24 +16,35 @@ public class Shop {
     public static void main(String[] args) {
 
         ProductManager pm = new ProductManager("en-GB") ;
-        Product p1 = pm.creatProduct( 101, "Tea", BigDecimal.valueOf (1.99), Rating.NOT_RATED, null) ;
-        p1 = pm.reviewProduct (101, Rating.FOUR_STAR, "Nice hot cup of tea") ;
-        Product p2 = pm.creatProduct( 102, "food", BigDecimal.valueOf (20), Rating.NOT_RATED, LocalDate.of(2027,10,10)) ;
-        Product p3 = pm.creatProduct( 103, "fish", BigDecimal.valueOf (70), Rating.NOT_RATED, LocalDate.of(2027,10,20)) ;
-        p3 = pm.reviewProduct (103, Rating.THREE_STAR, "Just add some lemon") ;
-        Product p4 = pm.creatProduct( 104, "potato", BigDecimal.valueOf (20), Rating.NOT_RATED, LocalDate.of(2027,10,10)) ;
-        pm.printProductReport (19);
-        pm.reviewProduct (19, Rating.THREE_STAR, "Just add some lemon") ;
+//        pm.creatProduct( 101, "Tea", BigDecimal.valueOf (1.99), Rating.NOT_RATED, null) ;
+//        pm.printProductReport(101);
+        pm.parseProduct("D,101,Tea,1.99,0,2021-10-4");
+        pm.parseReview("101,4,Nice cup of tea ");
+        pm.printProductReport(101);
 
-        p1 = pm.reviewProduct (101, Rating.TWO_STAR, "Rather weak tea") ;
-        p1 = pm.reviewProduct (101, Rating.FOUR_STAR, "Fine tea") ;
-        p1 = pm.reviewProduct (101, Rating.FOUR_STAR, "Good tea");
-        p1 = pm.reviewProduct (101, Rating.FIVE_STAR, "Perfect tea");
-        pm.printProductReport (101);
-        Comparator<Product> ratingSorter = (px, py)->py.getRating().ordinal()-px.getRating().ordinal();
-        Comparator<Product> priceSorter =(px,py)->py.getPrice().compareTo(px.getPrice());
+
+
+
+
+//
+//        Product p1 = pm.creatProduct( 101, "Tea", BigDecimal.valueOf (1.99), Rating.NOT_RATED, null) ;
+//        p1 = pm.reviewProduct (101, Rating.FOUR_STAR, "Nice hot cup of tea") ;
+//        Product p2 = pm.creatProduct( 102, "food", BigDecimal.valueOf (20), Rating.NOT_RATED, LocalDate.of(2027,10,10)) ;
+//        Product p3 = pm.creatProduct( 103, "fish", BigDecimal.valueOf (70), Rating.NOT_RATED, LocalDate.of(2027,10,20)) ;
+//        p3 = pm.reviewProduct (103, Rating.THREE_STAR, "Just add some lemon") ;
+//        Product p4 = pm.creatProduct( 104, "potato", BigDecimal.valueOf (20), Rating.NOT_RATED, LocalDate.of(2027,10,10)) ;
+//        pm.printProductReport (19);
+//        pm.reviewProduct (19, Rating.THREE_STAR, "Just add some lemon") ;
+//
+//        p1 = pm.reviewProduct (101, Rating.TWO_STAR, "Rather weak tea") ;
+//        p1 = pm.reviewProduct (101, Rating.FOUR_STAR, "Fine tea") ;
+//        p1 = pm.reviewProduct (101, Rating.FOUR_STAR, "Good tea");
+//        p1 = pm.reviewProduct (101, Rating.FIVE_STAR, "Perfect tea");
+//        pm.printProductReport (101);
+//        Comparator<Product> ratingSorter = (px, py)->py.getRating().ordinal()-px.getRating().ordinal();
+//        Comparator<Product> priceSorter =(px,py)->py.getPrice().compareTo(px.getPrice());
 //        pm.printProducts(ratingSorter.thenComparing(priceSorter));
 //        pm.printProducts(ratingSorter.thenComparing(priceSorter).reversed());
-        pm.printProducts( (px, py)->py.getRating().ordinal()-px.getRating().ordinal(),p->p.getPrice().floatValue()>2);
+//        pm.printProducts( (px, py)->py.getRating().ordinal()-px.getRating().ordinal(),p->p.getPrice().floatValue()>2);
         }
     }
--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -11,8 +11,10 @@ package labs.pm.data;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
 import java.util.*;
 import java.util.function.Predicate;
@@ -36,6 +38,9 @@ public class ProductManager {
 
     private static final Logger logger = Logger.getLogger(ProductManager.class.getName());
     private Map<Product, List<Review>> products= new HashMap<>();
+    private ResourceBundle config = ResourceBundle.getBundle("labs.pm.data.config");
+    private MessageFormat productFormat = new MessageFormat(config.getString("product.data.format"));
+    private MessageFormat reviewFormat = new MessageFormat(config.getString("review.data.format"));
 
     public ProductManager (String languageTag) {
         changeLocale (languageTag) ;
@@ -68,7 +73,8 @@ public class ProductManager {
             return products.keySet()
                     .stream()
                     .filter(p -> p.getId() == id)
-                    .findFirst().orElseThrow(()->new ProductManagerException("Product with id"+id+"not found"));
+                    .findFirst().orElseThrow(
+                            ()->new ProductManagerException("Product with id "+ id +" not found"));
 
     }
 
@@ -131,6 +137,39 @@ public class ProductManager {
                 .forEach(p->txt.append(formatter.formatProduct(p)+'\n'));
 
         System.out.println(txt);
+    }
+    public void parseReview  (String text) {
+        try {
+            Object [] values =reviewFormat.parse(text);
+            reviewProduct(Integer.parseInt((String)values[0]),
+                    Rateable.convert(Integer.parseInt((String)values[1])), (String)values[2]);
+        } catch (ParseException | NumberFormatException e) {
+            logger.log(Level.WARNING,"Error parsing review"+text,e);
+
+        }
+    }
+    public void parseProduct(String text){
+        try {
+            Object [] values =productFormat.parse(text);
+            int id = Integer.parseInt((String)values[1]);
+            String name = (String)values[2];
+            BigDecimal price = BigDecimal.valueOf(Double.parseDouble((String)values[3]));
+            Rating rating = Rateable.convert(Integer.parseInt((String)values[4]));
+            switch ((String)values[0]){
+                case "D":
+                    creatProduct(id,name,price,rating);
+                    break;
+                case "F":
+                    LocalDate bestBefore = LocalDate.parse((String)values[5]);
+                    creatProduct(id,name,price,rating,bestBefore);
+                    break;
+            }
+
+        } catch (ParseException | NumberFormatException | DateTimeParseException e) {
+            logger.log(Level.WARNING,"Error parsing product "
+                    +"text"+ e.getMessage());
+        }
+
     }
 //    public Map <String,String> getDiscount(){
 //        return products.keySet()
