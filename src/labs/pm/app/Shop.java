db5070b954b0c187acab9a4d0ab3e66f647d2e0a
kawtar <kawtar.karkour@gmail.com>
2025-03-10 22:23:19 +0000

    feature :Product Manager Memory Safety

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -11,11 +11,61 @@ import java.sql.Timestamp;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Shop {
     public static void main(String[] args) {
         ProductManager pm = ProductManager.getInstance();
+        AtomicInteger clientCount = new AtomicInteger(0);
+        Callable<String> client = ()->{
+            String clientId = "client "+ clientCount.incrementAndGet();
+            String threadName =Thread.currentThread().getName();
+            int productId = ThreadLocalRandom.current().nextInt(63)+101;
+            String languageTag = ProductManager.getSupportedLocales()
+                    .stream()
+                    .skip(ThreadLocalRandom.current().nextInt(4))
+                    .findFirst().get();
+            StringBuilder log = new StringBuilder();
+            log.append(clientId+"  "+threadName+"\n-\tstart of log\t-\n");
+            Product product = pm.reviewProduct (productId, Rating.FOUR_STAR, "Fine tea") ;
+            log.append((product==null)
+                    ?"\nProduct "+productId+"reviewed\n"
+                    :"\nProduct "+productId+"not reviewed\n");
+            pm.printProductReport(productId,languageTag,clientId);
+            log.append(clientId+" generate report for "+productId+"product");
+
+
+            log.append("\n-\tend of log\t-\n");
+            return "";
+        };
+        List<Callable<String>> clients = Stream.generate(()->client)
+                .limit(5)
+                .collect(Collectors.toList());
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        try {
+            List<Future<String>> results = executorService.invokeAll(clients);
+            executorService.shutdown();
+            results.stream().forEach(result->{
+                try {
+                    System.out.println(result.get());
+                } catch (InterruptedException |ExecutionException e) {
+                    Logger.getLogger(Shop.class.getName()).log(Level.SEVERE,"Error retrieving client log",e);
+                }
+            });
+        } catch (InterruptedException e) {
+            Logger.getLogger(Shop.class.getName()).log(Level.SEVERE,"Error invoking clients",e);
+        }
+
+
 //        ProductManager pm = new ProductManager("en-GB") ;
 //        pm.printProductReport(101);
 //        pm.creatProduct( 102, "honey", BigDecimal.valueOf (1.99), Rating.NOT_RATED, null) ;
